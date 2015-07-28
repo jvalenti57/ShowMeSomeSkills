7699ccab9e6501ed2286e96232c970657df705da
jvalenti57 <jvalenti57@gmail.com>
2015-07-28 17:15:28 -0400

    Real Quick Pass
    Did a quick pass during my lunch break.  Syncing to ensure I have the
    proper files at home.

---
+++ InterviewTest2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace InterviewTest1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var data = new Repo().All();
+            var dataCount = data.Count();
+            var i = 0;
+
+            string [] invoiceNumbers = data.Select(invoice => invoice.InvoiceNo).ToArray();
+            string[] companyNames = data.Select(invoice => invoice.CompanyName).ToArray();
+
+            while (i < dataCount)
+            {
+                Console.WriteLine("Invoice Number: " + invoiceNumbers[i] + "\nCompany Name: " + companyNames[i] + "\n");
+                i++;
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
---
+++ InterviewTest3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace InterviewTest1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var data = new Repo().All();
+
+            //This example does not include all required fields for output.
+            data.SelectMany(invoice => invoice.LineItems)
+                .ToList()
+                .ForEach(Console.WriteLine);
+
+            Console.ReadLine();
+        }
+    }
+}
